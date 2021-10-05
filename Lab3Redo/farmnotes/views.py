a84c86c1b42e87c48572c02650898cabfb05c531
uelpalmer12 <51063445+uelpalmer12@users.noreply.github.com>
2021-10-04 23:46:02 -0400

    templates

--- Lab3Redo/farmnotes/views.py
+++ Lab3Redo/farmnotes/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
 from .models import Field
 # Create your views here.
@@ -7,8 +7,9 @@ def index(request):
     context = {'latest_fields': latest_fields}
     return render(request, 'farmnotes/index.html', context)
 
-def notes (request, field_id):
-    return HttpResponse("You are looking at the notes related to field %s." % field_id)
+def notes(request, field_id):
+    field = get_object_or_404(Field, pk=field_id)
+    return render(request, 'farmnotes/notes.html', {'field': field})
 
 def observation(request, field_id, observation_id):
         return HttpResponse("You are looking at observation %s related to field %s"(observation_id, field_id))
