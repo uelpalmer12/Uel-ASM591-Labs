1971f9b9afdda2d63e7d53b68b31c6b87240dc4d
uelpalmer12 <51063445+uelpalmer12@users.noreply.github.com>
2021-10-04 21:13:25 -0400

    models created

--- Lab3/farmnotes/models.py
+++ Lab3/farmnotes/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
+
 # Create your models here.
+
+OBSERVATION_TYPES = [
+  ('weather', 'Weather'), 
+  ('crop', 'Crop'), 
+  ('soil', 'Soil'), 
+  ('water', 'Water'), 
+  ('pest', 'Pest'), 
+  ('other', 'Other'),
+]
+
+
+
+class Field (models.Model):
+    field_name= models.CharField(max_length=200)
+    date_planted= models.DateTimeField
+
+    def __str__(self):
+        return self.field_name
+
+class Observation(models.Model):
+    field = models.ForeignKey('field_name', on_delete=models.CASCADE)
+    observation_title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    observation_content = models.CharField(max_length=100)
+    observation_type = models.CharField(choices=OBSERVATION_TYPES)
+    observation_date = models.DateTimeField
+
+    def __str__(self):
+        return self.observation_title
