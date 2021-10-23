29f2b6fc0ef36003c2b234726049ec515f00be5f
uelpalmer12 <51063445+uelpalmer12@users.noreply.github.com>
2021-10-22 23:02:34 -0400

    Every thing work now link them

--- Lab4/acre/acrelog/urls.py
+++ Lab4/acre/acrelog/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("<int:field_id>", views.notes, name="notes"),
+    path("<int:field_id>/<int:job_id>/", views.job, name='job'),
 ]
--- Lab4/acre/acrelog/views.py
+++ Lab4/acre/acrelog/views.py
@@ -1,8 +1,19 @@
 from django.http.response import HttpResponse
-from django.shortcuts import render
-
+from django.shortcuts import get_object_or_404, render
+from .models import Field, Job
 # Create your views here.
 
-
 def index(request):
-  return render(request, 'acrelog/index.html')
+  return render(request, 'acrelog/index.html', {
+    "fields": Field.objects.all()
+  })
+
+
+def notes(request, field_id):
+  field = get_object_or_404(Field, pk=field_id)
+  return render(request, 'acrelog/notes.html', {'field': field, "jobs_done": field.job_done.all()})
+
+def job(request, job_id, field_id):
+  jobs = get_object_or_404(Job,pk=job_id)
+  field = get_object_or_404(Field, pk=field_id)
+  return render(request, 'acrelog/job.html', {'job': jobs, 'field': field})
